7d0739c2522ee7d987a7b937b1554b3f841f699b
alisajx <alisajx@126.com>
2018-01-05 18:34:20 +0800

    1

---
+++ src/components/category.vue
@@ -0,0 +1,21 @@
+<template>
+  <head class="category">
+
+  </head>
+</template>
+
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <head class="index">
+
+  </head>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
